79bfd78069260926403ea8e01ab51c3df6ab01b1
Danny Ayers <danny.ayers@gmail.com>
2021-10-07 15:19:28 +0200

    adding upload

--- js/millions.js
+++ js/millions.js
@@ -1,6 +1,43 @@
 var image = new Image()
 
-window.onload = function () {
+function submitForm () {
+  return false
+}
+
+window.onload = function drawDots () {
+  setup()
+}
+
+function prepareUploader () {
+  let imgInput = document.getElementById('imageInput')
+  imgInput.addEventListener('change', function (e) {
+    if (e.target.files) {
+      let imageFile = e.target.files[0] //here we get the image file
+      var reader = new FileReader()
+      reader.readAsDataURL(imageFile)
+      reader.onloadend = function (e) {
+        var myImage = new Image() // Creates image object
+        myImage.src = e.target.result // Assigns converted image to image object
+        myImage.onload = function (ev) {
+          var myCanvas = document.getElementById('myCanvas') // Creates a canvas object
+          var myContext = myCanvas.getContext('2d') // Creates a contect object
+          myCanvas.width = myImage.width // Assigns image's width to canvas
+          myCanvas.height = myImage.height // Assigns image's height to canvas
+          myContext.drawImage(myImage, 0, 0) // Draws the image on canvas
+          let imgData = myCanvas.toDataURL('image/jpeg', 0.75) // Assigns image base64 string in jpeg format to a variable
+        }
+      }
+    }
+  })
+}
+
+function setup () {
+  prepareUploader()
+  var size = document.getElementById('size').value
+  var borderProportion = document.getElementById('borderProportion').value
+  var multiplier = document.getElementById('multiplier').value
+  var iterations = document.getElementById('iterations').value
+
   var easel = document.getElementById('easel')
 
   var imageCanvas = document.createElement('canvas')
@@ -22,14 +59,40 @@ window.onload = function () {
   imageContext.fillStyle = '#ddddff'
   imageContext.fillRect(0, 0, imageCanvas.width, imageCanvas.height)
 */
-  image.src = 'images/dot.png'
+  image.src = 'images/sheep.png'
 
-  var size = 1000 // started crashing above 10,000
-  var border = size / 10
-  var multiplier = 10
+  // var size = 800 // started crashing above 10,000
+  //var borderProportion = 0.05
+  var border = size * borderProportion
+  // var multiplier = 10
+  // var iterations = 3
 
   imageCanvas.width = size
   imageCanvas.height = size
+
+  /*
+  function init () {
+    let imgInput = document.getElementById('imageInput')
+    imgInput.addEventListener('change', function (e) {
+      if (e.target.files) {
+        let imageFile = e.target.files[0] //here we get the image file
+        var reader = new FileReader()
+        reader.readAsDataURL(imageFile)
+        reader.onloadend = function (e) {
+          var myImage = new Image() // Creates image object
+          myImage.src = e.target.result // Assigns converted image to image object
+         myImage.onload = function (ev) {
+         
+         //   let imgData = myCanvas.toDataURL('image/jpeg', 0.75) // Assigns image base64 string in jpeg format to a variable
+          // }
+        }
+
+
+      }
+    }
+  }
+}
+*/
 
   image.onload = function (ev) {
     /* background colour
@@ -51,9 +114,9 @@ window.onload = function () {
     previousCanvas = imageCanvas
     previousContext = imageContext
 
-    for (var c = 1; c < 4; c++) {
+    for (var c = 0; c < iterations; c++) {
       var div = document.createElement('div')
-      var caption = (100 ** c).toString()
+      var caption = (100 ** (c + 1)).toString()
       var captionText = document.createTextNode(caption)
       var captionElement = document.createElement('caption')
       easel
